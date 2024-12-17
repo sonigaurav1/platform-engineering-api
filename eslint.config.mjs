587192c9d15693a7f480e54d3fa69a44ef6fb390
Bishal Rayamajhi <developerbishal01@gmail.com>
2024-12-17 22:09:09 +0545

    eslint9 configured successfully

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,65 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
+const languageOptions = {
+  globals: {
+    ...globals.node,
+    ...globals.jest,
+  },
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+  parser: tsParser,
+};
+
+const pluginsOptions = {
+  import: importPlugin,
+  'import/parsers': tsParser,
+  prettier: prettierPlugin,
+};
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['**/node_modules', '**/build', '**/husky', '**/log'] },
+  { languageOptions: { ...languageOptions } },
+  {
+    plugins: {
+      ...pluginsOptions,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-var': 'error',
+      semi: 'error',
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-multi-spaces': 'error',
+      'space-in-parens': 'error',
+      'no-multiple-empty-lines': 'error',
+      'prefer-const': 'error',
+      'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-// import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import express from 'express';
 
-// dotenv.config();
+import type { Express, Request, Response } from 'express';
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.debug(`[server]: Server is running at http://localhost:${port}`);
 });
-
-// just testing project setup
